4ff504a1923e204347df51bc7b65a124f3d58396
Mauricio Gomes <mauricio@edge14.com>
2013-12-06 10:41:30 -0500

    Renamed for clarity

--- lib/oauth/controllers/consumer_controller.rb
+++ lib/oauth/controllers/consumer_controller.rb
@@ -25,8 +25,8 @@ module Oauth
 
         unless @token
           if @consumer.ancestors.include?(Oauth2Token)
-            request_url = callback2_oauth_consumer_url + callback2_querystring
-            redirect_to @consumer.authorize_url(request_url)
+            callback_url = callback2_oauth_consumer_url + callback2_querystring
+            redirect_to @consumer.authorize_url(callback_url)
           else
             request_url = callback_oauth_consumer_url(params[:id]) + callback2_querystring
             @request_token = @consumer.get_request_token(request_url)
