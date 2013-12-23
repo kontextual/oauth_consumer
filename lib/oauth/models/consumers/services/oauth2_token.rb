4d147306f00044e8e2ff0e4881817ae1510cf3f5
Mauricio Gomes <mauricio@edge14.com>
2013-12-23 13:53:06 -0500

    RFC doesn't call for token_method to be included in token requests

--- lib/oauth/models/consumers/services/oauth2_token.rb
+++ lib/oauth/models/consumers/services/oauth2_token.rb
@@ -19,7 +19,7 @@ class Oauth2Token < ConsumerToken
   end
 
   def self.access_token(user, code, redirect_uri)
-    access_token = consumer.auth_code.get_token(code, { :redirect_uri => redirect_uri, :token_method => :post })
+    access_token = consumer.auth_code.get_token(code, { :redirect_uri => redirect_uri })
     find_or_create_from_access_token user, access_token
   end
 
